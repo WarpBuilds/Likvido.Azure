206366be843972ed6b3bbd352a08d19b516d3bb8
Søren Pedersen <sh.pedersen@gmail.com>
2023-10-19 11:55:06 +0200

    Fix test

--- src/UnitTests/EventGrid/EventGridServiceTests.cs
+++ src/UnitTests/EventGrid/EventGridServiceTests.cs
@@ -15,7 +15,7 @@ public class EventGridServiceTests
     [InlineData(5000, 1)]
     [InlineData(10000, 2)]
     [InlineData(15000, 3)]
-    [InlineData(20000, 7)]
+    [InlineData(20000, 4)]
     public async Task PublishAsync_WhenSendingManyEvents_TheEventsShouldBeSplitIntoBatches(int eventCount, int batches)
     {
         // Arrange
